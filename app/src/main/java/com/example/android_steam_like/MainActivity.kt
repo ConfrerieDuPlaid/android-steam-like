096b805a34d10405ae1df4a609de8ca9c193fa2c
Nathan-dev-dot <maerna1@gmail.com>
2022-12-06 22:16:11 +0100

    init android project and start acceuil activity

---
+++ app/src/main/java/com/example/android_steam_like/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.android_steam_like
+
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        getSupportActionBar()?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar()?.setDisplayShowCustomEnabled(true);
+        getSupportActionBar()?.setCustomView(R.layout.custom_action_bar);
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
+    }
+}
