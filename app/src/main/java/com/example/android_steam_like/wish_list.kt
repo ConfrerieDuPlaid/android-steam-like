0ca72bee85ca6ba52de4b61e39d4d00cb036891b
Nathan-dev-dot <maerna1@gmail.com>
2022-12-12 20:21:59 +0100

    empty wish list

---
+++ app/src/main/java/com/example/android_steam_like/wish_list.kt
@@ -0,0 +1,11 @@
+package com.example.android_steam_like
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class wish_list : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wish_list)
+    }
+}
