3d38502c1ca2beef5c39089e2a673845c67c98e2
Nathan-dev-dot <maerna1@gmail.com>
2023-01-04 18:50:23 +0100

    fix convlict

--- app/src/main/java/com/example/android_steam_like/game_detail.kt
+++ app/src/main/java/com/example/android_steam_like/game_detail.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.core.text.HtmlCompat.FROM_HTML_MODE_LEGACY
 import androidx.core.text.HtmlCompat.fromHtml
@@ -22,6 +23,7 @@ class game_detail : AppCompatActivity() {
         list.visibility  = View.GONE
         val appId = intent.getStringExtra("appId")
 
+        findViewById<ProgressBar>(R.id.progress_circular).visibility = View.GONE
 
         http_request("http://172.20.10.5:3000/game/$appId", this::displayDetail).start()
 
@@ -30,13 +32,17 @@ class game_detail : AppCompatActivity() {
 
     private fun setOnClickButton(appId: String?) {
         val list = findViewById<androidx.recyclerview.widget.RecyclerView>(R.id.avis)
-
+        val description = findViewById<TextView>(R.id.description)
         val descriptionButton = findViewById<Button>(R.id.description_button)
         val opinionsButton = findViewById<Button>(R.id.opinions)
+        val circularWaiting = findViewById<ProgressBar>(R.id.progress_circular)
 
         descriptionButton.setOnClickListener {
             opinionsButton.setBackgroundColor(Color.TRANSPARENT)
             descriptionButton.setBackgroundColor(0x5D63DE);
+            circularWaiting.visibility = View.GONE;
+            list.visibility  = View.GONE
+            description.visibility = View.VISIBLE
         }
 
         opinionsButton.setOnClickListener {
@@ -45,14 +51,28 @@ class game_detail : AppCompatActivity() {
 
             list.visibility  = View.VISIBLE
 
-            http_request("http://172.20.10.5:3000/comment/$appId", this::opinions).start()
+            description.visibility = View.GONE
+            circularWaiting.visibility = View.VISIBLE;
+            http_request("http://172.20.10.8:3000/comment/$appId", this::opinions).start()
 
         }
     }
 
     private fun opinions(res: String) {
+        val circularWaiting = findViewById<ProgressBar>(R.id.progress_circular)
         val jsonRes = JSONArray(res);
-        println("jsonRes = $jsonRes")
+
+        for (i in 0 until jsonRes.length()) {
+            this@game_detail.runOnUiThread(java.lang.Runnable {
+                try{
+                    println(jsonRes[i]);
+                    circularWaiting.visibility = View.GONE;
+
+                } catch (e: java.lang.Exception){
+
+                }
+            })
+        }
     }
 
 
