f99c5fd28de5a09cdb65f9602f1824f4c8de1083
Nathan-dev-dot <maerna1@gmail.com>
2023-01-25 18:20:34 +0100

    reset server config

--- app/src/main/java/com/example/android_steam_like/utils/ServerConfig.kt
+++ app/src/main/java/com/example/android_steam_like/utils/ServerConfig.kt
@@ -3,7 +3,7 @@ package com.example.android_steam_like.utils
 class ServerConfig {
     companion object {
         fun baseURL (): String {
-            return "http://172.20.10.8:3000";
+            return "http://141.94.222.41:3000";
         }
     }
 }
