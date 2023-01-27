b7c9be75a75fa2525a1f6fc36c9d5e210af200a2
Nathan-dev-dot <maerna1@gmail.com>
2023-01-27 11:43:02 +0100

    end of search layout

--- app/src/main/java/com/example/android_steam_like/SearchGameList.kt
+++ app/src/main/java/com/example/android_steam_like/SearchGameList.kt
@@ -2,6 +2,9 @@ package com.example.android_steam_like
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android_steam_like.entities.Game
@@ -16,7 +19,9 @@ class SearchGameList : AppCompatActivity() {
         setContentView(R.layout.search_game_list)
 
         val result = this.intent.getStringExtra("result");
-        displayGames(result!!);
+        displayGamesFromHome(result!!);
+
+        searchGame()
 
         findViewById<RecyclerView>(R.id.game_list).apply {
             layoutManager = LinearLayoutManager(this@SearchGameList)
@@ -24,9 +29,24 @@ class SearchGameList : AppCompatActivity() {
         };
     }
 
-    private fun displayGames(result: String) {
+    private fun searchGame() {
+        val editText = findViewById<EditText>(R.id.search_bar);
+        editText.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                Game.getGamesByName(editText.text.toString(), this::displayGamesFromHome)
+                false
+            } else {
+                false
+            }
+        }
+    }
+
+    private fun displayGamesFromHome(result: String) {
+        val resultNumber = findViewById<TextView>(R.id.result_number)
         val jsonResult = JSONArray(result);
+        this.games.clear()
         this@SearchGameList.runOnUiThread {
+            listAdapter.notifyDataSetChanged()
             for (i in 0 until jsonResult.length()) {
                 try {
                     val game = jsonResult.getJSONObject(i)
@@ -46,6 +66,7 @@ class SearchGameList : AppCompatActivity() {
                     println(e.message)
                 }
             }
+            resultNumber.text = "Nombre de résultats : " + this.games.size.toString()
         }
     }
 }
--- app/src/main/java/com/example/android_steam_like/utils/ServerConfig.kt
+++ app/src/main/java/com/example/android_steam_like/utils/ServerConfig.kt
@@ -3,7 +3,7 @@ package com.example.android_steam_like.utils
 class ServerConfig {
     companion object {
         fun baseURL (): String {
-            return "http://172.20.10.8:3000";
+            return "http://192.168.1.10:3000";
         }
     }
 }
