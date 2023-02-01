650af090585bcde891b228dababed8ce50cd6126
Sarah <sschlegel@myges.fr>
2023-02-01 17:00:47 +0100

    navigation wip

--- app/src/main/java/com/example/android_steam_like/Login.kt
+++ app/src/main/java/com/example/android_steam_like/Login.kt
@@ -1,12 +1,19 @@
 package com.example.android_steam_like
 
-import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.example.android_steam_like.entities.User
 import com.example.android_steam_like.entities.UserCredentials
 import com.example.android_steam_like.utils.CustomSteamAPI
@@ -16,10 +23,6 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class Login : AppCompatActivity() {
-    private var email: String = ""
-    private var password: String = ""
-
-    //todo : retirer la navbar
     //todo : message d'erreur on login fail
     //todo : surligner champs vides onclick login
 
@@ -27,63 +30,92 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
         supportActionBar?.hide()
-
-        setNavLinks()
-        setLoginListener()
-        setInputListeners()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, LoginFragment())
+            .commit()
     }
 
-    private fun login (res: User) {
-        User.setInstance(res)
-        intent = Intent(this, Home::class.java)
-        startActivity(intent)
-    }
+    class LoginFragment: Fragment () {
 
-    private fun setLoginListener () {
-        findViewById<Button>(R.id.login_button).setOnClickListener {
-            if (this.email != "" && this.password != "") {
-                val userCredentials = UserCredentials(email, password)
-                GlobalScope.launch(Dispatchers.Main) {
-                    GenericAPI.call(CustomSteamAPI.NetworkRequest::login, userCredentials, ::login)
+        private var email: String = ""
+        private var password: String = ""
+
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            return LayoutInflater.from(requireContext()).inflate(R.layout.login_fragment, container, false)
+        }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+        val navController = Navigation.findNavController(view)
+        println(navController)
+
+        println(findNavController())
+            setNavLinks(view)
+            setLoginListener(view)
+            setInputListeners(view)
+        }
+
+        private fun login (res: User) {
+            User.setInstance(res)
+            println(User.getInstance())
+//            LoginFragment
+//            intent = Intent(this, Home::class.java)
+//            startActivity(intent)
+        }
+
+        private fun setLoginListener (view: View) {
+            view.findViewById<Button>(R.id.login_button).setOnClickListener {
+                if (this.email != "" && this.password != "") {
+                    val userCredentials = UserCredentials(email, password)
+                    GlobalScope.launch(Dispatchers.Main) {
+                        GenericAPI.call(CustomSteamAPI.NetworkRequest::login, userCredentials, ::login)
+                    }
                 }
             }
         }
-    }
 
-    private fun setInputListeners () {
-        findViewById<EditText>(R.id.user_email).addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        private fun setInputListeners (view: View) {
+            view.findViewById<EditText>(R.id.user_email).addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-            }
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                }
 
-            override fun afterTextChanged(s: Editable?) {
-                email = s.toString()
-            }
-        })
-        findViewById<EditText>(R.id.user_password).addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun afterTextChanged(s: Editable?) {
+                    email = s.toString()
+                }
+            })
+            view.findViewById<EditText>(R.id.user_password).addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-            }
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                }
 
-            override fun afterTextChanged(s: Editable?) {
-                password = s.toString()
-            }
-        })
-    }
-
-    private fun setNavLinks () {
-        findViewById<Button>(R.id.new_account_button).setOnClickListener {
-            intent = Intent(this, Signin::class.java)
-            if (email != "") intent.putExtra("email", email)
-            if (password != "") intent.putExtra("password", password)
-            startActivity(intent)
+                override fun afterTextChanged(s: Editable?) {
+                    password = s.toString()
+                }
+            })
         }
-        findViewById<Button>(R.id.forgotten_password_button).setOnClickListener {
-            intent = Intent(this, ForgottenPassword::class.java)
-            if (email != "") intent.putExtra("email", email)
-            startActivity(intent)
+
+        private fun setNavLinks (view: View) {
+            view.findViewById<Button>(R.id.new_account_button).setOnClickListener {
+                findNavController().navigate(
+                    `Login$LoginFragmentDirections`.actionLoginFragmentToSigninFragment()
+                )
+//                intent = Intent(this, Signin::class.java)
+//                if (email != "") intent.putExtra("email", email)
+//                if (password != "") intent.putExtra("password", password)
+//                startActivity(intent)
+            }
+            view.findViewById<Button>(R.id.forgotten_password_button).setOnClickListener {
+//                intent = Intent(this, ForgottenPassword::class.java)
+//                if (email != "") intent.putExtra("email", email)
+//                startActivity(intent)
+            }
         }
     }
 }
--- app/src/main/java/com/example/android_steam_like/Signin.kt
+++ app/src/main/java/com/example/android_steam_like/Signin.kt
@@ -2,10 +2,14 @@ package com.example.android_steam_like
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.android_steam_like.entities.User
 import com.example.android_steam_like.entities.UserSignupBody
 import com.example.android_steam_like.utils.CustomSteamAPI
@@ -15,6 +19,19 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class Signin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signin)
+        supportActionBar?.hide()
+
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.signin_container, SigninFragment())
+//            .commit()
+    }
+}
+
+class SigninFragment: Fragment() {
     private var usernameInput: EditText? = null
     private var emailInput: EditText? = null
     private var passwordInput: EditText? = null
@@ -22,26 +39,31 @@ class Signin : AppCompatActivity() {
     private var email: String = ""
     private var password: String = ""
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.signin)
-        supportActionBar?.hide()
-
-        setDataFromIntent()
-        setSigninListener()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return LayoutInflater.from(requireContext()).inflate(R.layout.signin_fragment, container, false)
     }
 
-    private fun setDataFromIntent () {
-        this.usernameInput = findViewById(R.id.username)
-        this.emailInput = findViewById(R.id.user_email)
-        this.passwordInput = findViewById(R.id.user_password)
-        this.passwordVerifInput = findViewById(R.id.user_password_verification)
-        if (this.intent.hasExtra("email")) {
-            this.email = this.intent.getStringExtra("email")!!
-        }
-        if (this.intent.hasExtra("password"))
-            this.password = this.intent.getStringExtra("password")!!
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setDataFromIntent(view)
+        setSigninListener(view)
+
+    }
+
+    private fun setDataFromIntent (view: View) {
+        this.usernameInput = view.findViewById(R.id.username)
+        this.emailInput = view.findViewById(R.id.user_email)
+        this.passwordInput = view.findViewById(R.id.user_password)
+        this.passwordVerifInput = view.findViewById(R.id.user_password_verification)
+//            if (this.intent.hasExtra("email")) {
+//                this.email = this.intent.getStringExtra("email")!!
+//            }
+//            if (this.intent.hasExtra("password"))
+//                this.password = this.intent.getStringExtra("password")!!
         if (this.email != "") {
             this.emailInput?.setText(email, TextView.BufferType.EDITABLE)
         }
@@ -53,12 +75,12 @@ class Signin : AppCompatActivity() {
 
     private fun signin (res: User) {
         User.setInstance(res)
-        intent = Intent(this, Home::class.java)
-        startActivity(intent)
+//            intent = Intent(this, Home::class.java)
+//            startActivity(intent)
     }
 
-    private fun setSigninListener () {
-        findViewById<Button>(R.id.signin_button).setOnClickListener {
+    private fun setSigninListener (view: View) {
+        view.findViewById<Button>(R.id.signin_button).setOnClickListener {
             val username = usernameInput?.text.toString()
             email = emailInput?.text.toString()
             password = passwordInput?.text.toString()
